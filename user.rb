0310aee37308e985ed82b5df8e3f1c3df16283d9
Sergey Filippov <sergey.filippov.89@inbox.ru>
2020-09-08 20:34:55 +0800

    added user.rb

---
+++ user.rb
@@ -0,0 +1,44 @@
+class User < ApplicationRecord
+  DEFAULT_NAME = "#{Rails.application.class.module_parent} User".freeze
+
+  before_validation :set_defaults
+
+  def token
+    TokenService.encode('user' => id)
+  end
+
+  def index_hash
+    h = standard_hash
+    # Add new attributes here as shown below:
+    # h[:images] = images
+    return h
+  end
+
+  def show_hash
+    h = standard_hash
+    # Add new attributes here as shown below:
+    # h[:images] = images
+    return h
+  end
+
+  def standard_hash
+    {
+      id: id,
+      name: name,
+      city: city,
+      wechat: wechat,
+      phone: phone,
+      email: email,
+      gender: gender,
+      admin: admin
+    }
+  end
+
+
+
+  private
+
+  def set_defaults
+    self.name = DEFAULT_NAME if self.name.blank?
+  end
+end
